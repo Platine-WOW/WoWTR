6921585e4974078381ab153b7dabf504c9e25a0b
Platine-WOW <platine.wow@gmail.com>
2024-03-28 16:38:30 +0100

    wrong name of variable on line 695

--- WoW_Tooltips.lua
+++ WoW_Tooltips.lua
@@ -692,7 +692,7 @@ function ST_GameTooltipOnShow()
       if ((ST_orygText or (ST_nh==1)) and (ST_PM["saveNW"]=="1")) then
          for _, ST_origin in ipairs(ST_orygText) do   
             ST_hash = StringHash(ST_UsunZbedneZnaki(ST_origin));
-            if (string.sub(ST_orygText,1,11) ~= '|A:raceicon') then
+            if (string.sub(ST_origin,1,11) ~= '|A:raceicon') then
                if ((not ST_TooltipsHS[ST_hash]) and (string.find(ST_origin," ")==nil)) then    -- i nie jest to tekst tłumaczenia (twarda spacja)
                   ST_PH[ST_hash]=ST_prefix.."@"..ST_PrzedZapisem(ST_origin);
                end
